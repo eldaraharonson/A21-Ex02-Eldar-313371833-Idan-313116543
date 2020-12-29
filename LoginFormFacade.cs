9edc078cf918a0e57deff9ccdac9a6a6a90219c0
Eldar Aharonson <eaharonson1806@gmail.com>
2020-12-29 18:59:36 +0200

    added facade to loginForm

---
+++ LoginFormFacade.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FacebookWrapper;
+
+namespace B20_Ex01_Eldar_313371833_Idan_313116543
+{
+    public class LoginFormFacade
+    {
+        public static LoginResult LoginToFacebook(string i_AppId, params string[] i_AccessableFacebookAttributes)
+        {
+            return FacebookService.Login(i_AppId, i_AccessableFacebookAttributes);
+        }
+    }
+}
